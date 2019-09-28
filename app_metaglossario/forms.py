20e16b8f30edbad044a3c415ec16471ad304901a
Tommaso Sansone <tommaso.sansone@mail.polimi.it>
2019-09-28 12:05:33 +0200

    modificato modello bilaterale in modello unico

--- app_metaglossario/forms.py
+++ app_metaglossario/forms.py
@@ -5,8 +5,7 @@ from .models import glossary_entry
 class glossary_entry_form(forms.ModelForm):
         
         
-        
     class Meta:
         model=glossary_entry
-        fields=["Lemma_it", "Lemma_ch", "Acronimo_it", "Acronimo_ch", "Definizione_it", "Definizione_ch", "Ambito_riferimento_it", "Ambito_riferimento_ch", "Autore_definizione_it", "Autore_definizione_ch", "Posizione_definizione_it", "Posizione_definizione_ch", "Url_definizione_it", "Url_definizione_ch", "Titolo_documento_fonte_it", "Titolo_documento_fonte_ch", "Autore_documento_fonte_it", "Autore_documento_fonte_ch", "Host_documento_fonte_it", "Host_documento_fonte_ch", "Url_documento_fonte_it", "Url_documento_fonte_ch", "Data_inserimento_entry", "Id_statico_entry", "Admin_approval_switch"]
+        fields=["Lemma", "Acronimo", "Definizione", "Ambito_riferimento", "Autore_definizione", "Posizione_definizione", "Url_definizione", "Titolo_documento_fonte", "Autore_documento_fonte", "Host_documento_fonte", "Url_documento_fonte", "Data_inserimento_entry", "Id_statico_entry", "Admin_approval_switch"]
 
