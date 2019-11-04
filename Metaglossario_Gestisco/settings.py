466dea4fa0986ada696c49b1e4948149ae62e2bc
Tommaso Sansone <tommaso.sansone@mail.polimi.it>
2019-11-04 10:57:36 +0100

    tentativo aumento max linee manage admin section in settings py

--- Metaglossario_Gestisco/settings.py
+++ Metaglossario_Gestisco/settings.py
@@ -147,3 +147,5 @@ IMPORT_EXPORT_USE_TRANSACTIONS = True
 # aggiunto per indicare la directory dei files salvati dallìupload degli utenti
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploaded_files')
 MEDIA_URL = '/uploaded_files/'
+
+DATA_UPLOAD_MAX_NUMBER_FIELDS = 13000 # higher than the count of fields
