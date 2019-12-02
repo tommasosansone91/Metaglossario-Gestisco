3d3b53fd43f0a387630ea1222502a2d9f40449eb
Tommaso Sansone <tommaso.sansone@mail.polimi.it>
2019-12-02 19:19:32 +0100

    cambiato SR per stampare tabelle relazionali e Things in excel in directory locale e non riempire più i modelli di Things e tabelle relazionali

--- app_metaglossario/node_models.py
+++ app_metaglossario/node_models.py
@@ -36,28 +36,60 @@ class model_node(models.Model):
 
     # elementi connessi
     connected_Lemma = ArrayField(models.CharField(max_length=256), blank=True, null=True)
+    connected_Lemma_ID = ArrayField(models.CharField(max_length=10), blank=True, null=True)
+
     connected_Acronimo = ArrayField(models.CharField(max_length=25), blank=True, null=True)
+    connected_Acronimo_ID = ArrayField(models.CharField(max_length=10), blank=True, null=True)
+
     connected_Definizione = ArrayField(models.TextField(), blank=True, null=True)
+    connected_Definizione_ID = ArrayField(models.CharField(max_length=10), blank=True, null=True)
+
+
     connected_Ambito_riferimento = ArrayField(models.CharField(max_length=256), blank=True, null=True)
+    connected_Ambito_riferimento_ID = ArrayField(models.CharField(max_length=10), blank=True, null=True)
+
     connected_Autore_definizione = ArrayField(models.TextField(), blank=True, null=True)
+    connected_Autore_definizione_ID = ArrayField(models.CharField(max_length=10), blank=True, null=True)
+
     connected_Posizione_definizione = ArrayField(models.TextField(), blank=True, null=True) 
+    connected_Posizione_definizione_ID = ArrayField(models.CharField(max_length=10), blank=True, null=True)
+
     connected_Url_definizione = ArrayField(models.URLField(max_length=400), blank=True, null=True)
+    connected_Url_definizione_ID = ArrayField(models.CharField(max_length=10), blank=True, null=True)
+
     connected_Titolo_documento_fonte = ArrayField(models.TextField(), blank=True, null=True)
+    connected_Titolo_documento_fonte_ID = ArrayField(models.CharField(max_length=10), blank=True, null=True)
+
     connected_Url_documento_fonte = ArrayField(models.URLField(max_length=400), blank=True, null=True)
+    connected_Url_documento_fonte_ID = ArrayField(models.CharField(max_length=10), blank=True, null=True)
+
     connected_Autore_documento_fonte = ArrayField(models.TextField(), blank=True, null=True) 
+    connected_Autore_documento_fonte_ID = ArrayField(models.CharField(max_length=10), blank=True, null=True)
+
     connected_Host_documento_fonte = ArrayField(models.TextField(), blank=True, null=True)
+    connected_Host_documento_fonte_ID = ArrayField(models.CharField(max_length=10), blank=True, null=True)
+
     connected_Url_documento_fonte = ArrayField(models.URLField(max_length=400), blank=True, null=True)
+    connected_Url_documento_fonte_ID = ArrayField(models.CharField(max_length=10), blank=True, null=True)
+
     connected_Commento_entry = ArrayField(models.TextField(), blank=True, null=True)
+    connected_Commento_entry_ID = ArrayField(models.CharField(max_length=10), blank=True, null=True)
+
     connected_Data_inserimento_entry = ArrayField(models.DateField(),blank=False, null=False, default=timezone.now().date())
+    connected_Data_inserimento_entry_ID = ArrayField(models.CharField(max_length=10), blank=True, null=True)
+
     connected_Id_statico_entry = ArrayField(models.CharField(max_length=256), blank=False, null=False, default="ITCH00000")
+    connected_Id_statico_entry_ID = ArrayField(models.CharField(max_length=10), blank=True, null=True)
+
     connected_Admin_approval_switch = ArrayField(models.CharField(max_length=30),blank=False, null=False)
+    connected_Admin_approval_switch_ID = ArrayField(models.CharField(max_length=10), blank=True, null=True)
     
 
     class Meta:
-        ordering = ['Entita', 'Thing', 'ID']
+        ordering = ['Entita', 'centered_Thing', 'centered_ID']
 
     def __str__(self):                
-        return  "%s [ %s ]"  %  (self.Thing, self.ID)
+        return  "%s [ %s ]"  %  (self.centered_Thing, self.centered_ID)
 
 
 
