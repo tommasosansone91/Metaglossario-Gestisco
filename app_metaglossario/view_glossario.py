3802719f233806a5552d660c4de9a67ac1619500
tommasosansone91 <tommaso.sansone@mail.polimi.it>
2020-02-05 11:48:03 +0100

    aggiunta ricerca avanzata al glossario, aggiunte bandierine al glossario

--- app_metaglossario/view_glossario.py
+++ app_metaglossario/view_glossario.py
@@ -19,13 +19,45 @@ def glossario(request):
 
     all_entries = prepared_terminology.objects.all() #funziona lo stesso anche se dice Class 'glossary_entry' has no 'objects' memberpylint(no-member)
 
-    # se la query è stata fatta
+    # se la query è stata fatta, query contiene il termine inserito dall'utente
     if query:
 
         query = request.GET.get('q') #q è variabile risultante dalla query del database
 
-        selected_entries = prepared_terminology.objects.filter(Q(Acronimo_it__icontains=query)|Q(Ambito_riferimento_it__icontains=query)|Q(Autore_definizione_it__icontains=query)|Q(Autore_documento_fonte_it__icontains=query)|Q(Definizione_it__icontains=query)|Q(Host_documento_fonte_it__icontains=query)|Q(Lemma_it__icontains=query)|Q(Posizione_definizione_it__icontains=query)|Q(Titolo_documento_fonte_it__icontains=query)|Q(Url_definizione_it__icontains=query)|Q(Url_documento_fonte_it__icontains=query)|Q(Acronimo_ch__icontains=query)|Q(Ambito_riferimento_ch__icontains=query)|Q(Autore_definizione_ch__icontains=query)|Q(Autore_documento_fonte_ch__icontains=query)|Q(Definizione_ch__icontains=query)|Q(Host_documento_fonte_ch__icontains=query)|Q(Lemma_ch__icontains=query)|Q(Posizione_definizione_ch__icontains=query)|Q(Titolo_documento_fonte_ch__icontains=query)|Q(Url_definizione_ch__icontains=query)|Q(Url_documento_fonte_ch__icontains=query)| Q(Data_inserimento_entry__icontains=query)| Q(Id_statico_entry__icontains=query))
-        # Q(Acronimo__icontains=query dice quali sono i campi in cui cercare l'input specificato dall'utente
+        #filtro intelligente: 
+        if query.lower().startswith("acronimo:"):
+            query = query[9:]
+            print("Ricerca '%s' tra gli acronimi" % query)
+            selected_entries = prepared_terminology.objects.filter(Q(Acronimo_it__icontains=query)|Q(Acronimo_ch__icontains=query))
+
+        #filtro intelligente: 
+        elif query.lower().startswith("lemma:"):
+            query = query[6:]
+            print("Ricerca '%s' tra i lemmi" % query)
+            selected_entries = prepared_terminology.objects.filter(Q(Lemma_it__icontains=query)|Q(Lemma_ch__icontains=query))
+
+        #filtro intelligente: 
+        elif query.lower().startswith("definizione:"):
+            query = query[12:]
+            print("Ricerca '%s' tra le definizioni" % query)
+            selected_entries = prepared_terminology.objects.filter(Q(Definizione_it__icontains=query)|Q(Definizione_ch__icontains=query))
+
+        #filtro intelligente: 
+        elif query.lower().startswith("fonte:"):
+            query = query[6:]
+            print("Ricerca '%s' tra gli autori di definizioni e di documenti fonte, tra i titoli dei documenti fonte e tra gli host dei documenti fonte." % query)
+            selected_entries = prepared_terminology.objects.filter(Q(Autore_definizione_it__icontains=query)|Q(Autore_documento_fonte_it__icontains=query)|Q(Host_documento_fonte_it__icontains=query)|Q(Titolo_documento_fonte_it__icontains=query)|Q(Autore_definizione_ch__icontains=query)|Q(Autore_documento_fonte_ch__icontains=query)|Q(Host_documento_fonte_ch__icontains=query)|Q(Titolo_documento_fonte_ch__icontains=query)    )
+
+        elif query.lower().startswith("url:"):
+            query = query[4:]
+            print("Ricerca '%s' gli URL" % query)
+            selected_entries = prepared_terminology.objects.filter(Q(Url_documento_fonte_it__icontains=query)|Q(Url_documento_fonte_ch__icontains=query))
+
+
+        else:        
+            print("Ricerca '%s' in tutti i campi" % query)
+            selected_entries = prepared_terminology.objects.filter(Q(Acronimo_it__icontains=query)|Q(Ambito_riferimento_it__icontains=query)|Q(Autore_definizione_it__icontains=query)|Q(Autore_documento_fonte_it__icontains=query)|Q(Definizione_it__icontains=query)|Q(Host_documento_fonte_it__icontains=query)|Q(Lemma_it__icontains=query)|Q(Posizione_definizione_it__icontains=query)|Q(Titolo_documento_fonte_it__icontains=query)|Q(Url_definizione_it__icontains=query)|Q(Url_documento_fonte_it__icontains=query)|Q(Acronimo_ch__icontains=query)|Q(Ambito_riferimento_ch__icontains=query)|Q(Autore_definizione_ch__icontains=query)|Q(Autore_documento_fonte_ch__icontains=query)|Q(Definizione_ch__icontains=query)|Q(Host_documento_fonte_ch__icontains=query)|Q(Lemma_ch__icontains=query)|Q(Posizione_definizione_ch__icontains=query)|Q(Titolo_documento_fonte_ch__icontains=query)|Q(Url_definizione_ch__icontains=query)|Q(Url_documento_fonte_ch__icontains=query)| Q(Data_inserimento_entry__icontains=query)| Q(Id_statico_entry__icontains=query))
+            # Q(Acronimo__icontains=query dice quali sono i campi in cui cercare l'input specificato dall'utente
 
         # Pagination
         paginator = Paginator(selected_entries, 10) # Show 25 contacts per page
--- static/js/popup_alert.js
+++ static/js/popup_alert.js
@@ -1,3 +1,12 @@
 function popup_tempo_api_glossario() {
     alert("Il caricamento dell'intero glossario può richiedere diversi minuti. Si prega di attendere.");
 }
+
+function popup_istruzioni_ricerca_avanzata(){
+    alert("
+    Per cercare un termine solo tra gli acronimi inserisci 'acronimo:' e poi il termine desiderato (es. acronimo:coc).\n\nPer cercare un termine solo tra i lemmi inserisci 'lemma:' e poi il termine desiderato (es. lemma:condotta).\n\nPer cercare un termine solo tra le definizioni inserisci 'definizione:' e poi il termine desiderato (es. definizione:di protezione civile).\n\nPer cercare un termine tra gli autori di definizioni e di documenti fonte, tra i titoli dei documenti fonte e tra gli host dei documenti fonte inserisci 'fonte:' e poi il termine desiderato (es. fonte:cantone ticino).\n\nPer cercare un termine solo tra gli URL inserisci 'url:' e poi il termine desiderato (es. url:http://www.gazzettaufficiale.it).\n\nNota: non inserire spazi tra i ':' e il termine desiderato.");
+
+    // Per cercare un termine solo tra i lemmi inserisci 'lemma:' e poi il termine desiderato (es. lemma:condotta).\n\n
+
+
+}
